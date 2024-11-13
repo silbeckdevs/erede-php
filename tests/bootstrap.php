1ff8bd3aa6bec57738b03b7a44dce11ec48e3019
Edson Nascimento <edson27bn@gmail.com>
2024-11-13 11:30:20 -0300

    ✅ Adicionar timezone `America/Sao_Paulo` nos testes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,4 +3,6 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 // for local development copy tests/config/env.test.php.example to tests/config/env.test.php and add your credentials
-include dirname(__DIR__) . '/tests/config/env.test.php';
+@include dirname(__DIR__) . '/tests/config/env.test.php';
+
+date_default_timezone_set('America/Sao_Paulo');
