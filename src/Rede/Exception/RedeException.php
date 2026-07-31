d076de9bdbef793f8fb7e6ed63e4cd64dab0f70d
edson-nascimento <edson27bn@gmail.com>
2026-07-31 13:53:43 -0300

    feat: extend RedeException to include transaction and HTTP response details

--- src/Rede/Exception/RedeException.php
+++ src/Rede/Exception/RedeException.php
@@ -2,6 +2,28 @@
 
 namespace Rede\Exception;
 
+use Rede\Http\RedeResponse;
+use Rede\Transaction;
+
 class RedeException extends \RuntimeException
 {
+    public function __construct(
+        string $message = '',
+        int $code = 0,
+        ?\Throwable $previous = null,
+        private ?Transaction $transaction = null,
+        private ?RedeResponse $httpResponse = null,
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getTransaction(): ?Transaction
+    {
+        return $this->transaction;
+    }
+
+    public function getHttpResponse(): ?RedeResponse
+    {
+        return $this->httpResponse ?? $this->transaction?->getHttpResponse() ?? null;
+    }
 }
